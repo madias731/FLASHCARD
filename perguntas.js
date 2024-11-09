5f9a799a2918a785a35a5cd717ff2d49435d6391
madias731 <144125273+madias731@users.noreply.github.com>
2024-11-09 20:21:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criaCartao(
+    'Escola',
+    'Qual sua matéria favorita Maria?',
+    'Minha matéria favorita é matemática'
+)
+criaCartao(
+    'Cores',
+    'Qual sua cor favorita Maria?',
+    'Minha cor favorita é roxo'
+)
+criaCartao(
+    'Stray Kids',
+    'Qual a música preferida de Stray Kids, por Maria?',
+    'A música preferida de Maria é Fam'
+)
